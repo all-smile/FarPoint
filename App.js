37fc879549e15650b7c798f6a16453956d72e441
xiaojuntao <find_onepiece@163.com>
2022-06-13 18:53:06 +0800

    feat: 图片资源整理、目录结构划分、首页开发

--- App.js
+++ App.js
@@ -1,87 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import axios from 'axios';
+import { View, Text, Image, Dimensions, ImageBackground, StyleSheet } from 'react-native'
+import { formatMoneyWithReg } from './src/lib/utils'
+const screenW = Math.round(Dimensions.get('window').width) // 360
+const screenH = Math.round(Dimensions.get('window').height) // 750
+// alert('screenH=' + screenH + 'screenW=' + screenW);
 
-import React from 'react';
-import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Image,
-  Dimensions
-} from 'react-native';
+axios.get("https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata").then(console.log)
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+console.log('调试');
+const App = () => <View style={styles.appContainer}>
+  {/* <Image style={{ width: '100%', height: '100%' }} source={require('./src/img/collection/11.jpg')}></Image> */}
+  <ImageBackground style={styles.homeBg} source={require('./src/img/collection/11.jpg')}>
+    <View style={styles.homeSloganWare}>
+      <Text style={styles.homeSlogan}>给</Text>
+      <Text style={styles.homeSloganBig}>未来</Text>
+      <Text style={styles.homeSlogan}>写</Text>
+      <Text style={styles.homeSlogan}>封</Text>
+      <Text style={styles.homeSlogan}>信</Text>
+      <Text style={styles.homeAddUp}>已寄出 {formatMoneyWithReg(378562)} 封信</Text>
+    </View>
+  </ImageBackground>
+</View>
 
-const screenWidth = Math.round(Dimensions.get('window').width)
-const screenHeight = Math.round(Dimensions.get('window').height)
-
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Image
-            style={{ width: 100, height: 200 }}
-            source={require('./static/img/cat.jpg')}
-          />
-          {/* <Image
-            style={{ width: "100%", height: "100%" }}
-            source={require('./static/img/gloading.gif')}
-          /> */}
-          <Text style={{ color: "#333", fontSize: 20 }}>远点123</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  appContainer: {
+    width: '100%',
+    height: '100%'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  homeBg: {
+    width: '100%',
+    height: '100%'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  homeSloganWare: {
+    padding: 40
   },
-  highlight: {
-    fontWeight: '700',
+  homeSlogan: {
+    color: "#012B6D",
+    fontSize: 52
+  },
+  homeSloganBig: {
+    color: "#012B6D",
+    fontSize: 62
+  },
+  homeAddUp: {
+    color: "#012B6D",
+    fontSize: 14
   },
 });
-
-export default App;
--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// 调试-捕获网络请求
+// GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest
 
 AppRegistry.registerComponent(appName, () => App);
