0a144f8e9f26d81c4261af877550a1e89f67a723
肖俊涛 <allblue95@126.com>
2022-06-16 19:26:30 +0800

    feat: 完善提交信息验证功能

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,12 @@
 const checkType = (header) => {
   header = `${header}`;
-  console.log('checkType header====', header);
-  const enumType = ['feat', 'fix', 'style', 'chore', 'test', 'ci', 'refactor', 'revert'];
+  const enumType = ['feat', 'fix', 'style', 'chore', 'test', 'ci', 'refactor', 'revert', 'reformat', 'docs'];
   const realType = header.split(':')[0];
   return enumType.includes(realType);
 };
 
 const checkSubject = (header) => {
   header = `${header}`;
-  console.log('checkSubject header====', header);
   const realSubject = header.split(':')[1];
   if (!realSubject) {
     return false;
@@ -19,29 +17,13 @@ const checkSubject = (header) => {
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [2, 'always'], // body换行
-    'header-max-length': [2, 'never', 72], // header 最长72
+    // 'body-leading-blank': [2, 'always'], // body换行
+    // 'header-max-length': [2, 'never', 72], // header 最长72
     'type-enum-rule': [2, 'never'],
+    'subject-enum-rule': [2, 'never'],
     'type-enum': [0, 'never'],
     'type-empty': [0, 'always'],
     'subject-empty': [0, 'always'],
-    // 'type-enum': [
-    //   2,
-    //   'always',
-    //   [
-    //     'add', // 创建功能
-    //     'del', // 删除功能
-    //     'fix', // 解决问题
-    //     'bump', // 修改某个版本号
-    //     'conf', // 配置文件修改
-    //     'refactor', // 必须进行重构的代码
-    //     'reformat', // 代码格式化
-    //     'optimize', // 代码性能优化
-    //     'doc', // 文档构建与修改
-    //     'start', // 开始做某事，比如创建分支等
-    //     'end', // 结束做某事，比如删除分支等
-    //   ],
-    // ],
   },
   plugins: [
     {
