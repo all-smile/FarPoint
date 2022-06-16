51ccb72d1f08f5b12857f69b10ff5b1e8244ee1e
肖俊涛 <allblue95@126.com>
2022-06-16 17:05:17 +0800

    chore: 修改开发配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,11 @@ module.exports = {
     "space-in-parens": 2, // 强制在圆括号内使用一致的空格
     "no-var": 2, // 要求使用 let 或 const 而不是 var,
     "no-unused-vars": 2, // 禁止出现未使用过的变量
-    "react/prop-types": 0 // 防止在react组件定义中缺少props验证
+    "react/prop-types": 0, // 防止在react组件定义中缺少props验证
+    "indent": ['error', 2], //缩进规则为2个空格
+    'react/jsx-indent': ['error', 2], //缩进规则为2个空格
+    'react/jsx-indent-props': ['error', 2], //缩进规则为2个空格
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //在JS文件中允许存在JSX语法
+    'global-require': 'off', //image指定source时要用require语句
   }
 };
