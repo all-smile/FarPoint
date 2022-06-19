613fad0bc5a9197ca9de44bb7edee102518de885
肖俊涛 <allblue95@126.com>
2022-06-19 22:14:30 +0800

    feat: 首页svg图标开发

--- App.js
+++ App.js
@@ -3,21 +3,13 @@ import React from 'react';
 import { View, Text, ImageBackground, StyleSheet, ScrollView, SafeAreaView /* ,StatusBar */ } from 'react-native';
 import RootStore from './src/mobx';
 import { Provider } from 'mobx-react';
-import { /* Svg, */ /* Circle, */ SvgUri, SvgXml } from 'react-native-svg';
-// import { SvgXml } from "react-native-svg";
 // import AppNavigator from "./src/navigation";
+import Svg from './src/components/Svg';
 import Btn from './src/components/Btn';
-import SymbolSvg from './src/assets/svgs/symbol.svg';
 import { width, height, px2dp } from './src/lib/utils/screenKits';
 import { formatMoneyWithReg } from './src/lib/utils/common';
 
-const xml = `<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1655121069194" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4898" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"><defs><style type="text/css"></style></defs><path d="M0 778.112L620.672 485.12 0 192l164.288 293.12z" fill="#1F7BFF" fill-opacity=".1" p-id="4899"></path><path d="M341.312 778.112l620.672-293.056L341.312 192 505.6 485.12z" fill="#1F7BFF" p-id="4900"></path></svg>`;
-
-// const req = require.context('./src/assets/svgs', false, /\.svg$/);
-// console.log('req=', req);
-
 console.log(width, height, px2dp(200));
-console.log('SymbolSvg=', typeof SymbolSvg);
 // axios.get("https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata").then(console.log)
 
 console.log('调试');
@@ -36,12 +28,9 @@ const App = () => (
             <Text style={styles.homeSlogan}>信</Text>
             <Text style={styles.homeAddUp}>已寄出 {formatMoneyWithReg(378562)} 封信</Text>
             <Btn></Btn>
-            <SvgUri width={px2dp(150)} height={px2dp(100)} uri="https://www.w3school.com.cn/svg/circle1.svg" />
-            <SvgXml xml={xml} width={px2dp(20)} height={px2dp(20)} />
-            {/* <SymbolSvg width={200} height={200}></SymbolSvg> */}
-            {/* <Svg height="50%" width="50%" viewBox="0 0 100 100">
-            <Circle cx="50" cy="50" r="50" stroke="purple" strokeWidth=".5" fill="violet" />
-          </Svg> */}
+            <View style={styles.writeBtnWare}>
+              <Svg icon="pen02" size={px2dp(45)} style={styles.writeBtn}></Svg>
+            </View>
           </View>
         </ImageBackground>
       </Provider>
@@ -69,6 +58,8 @@ const styles = StyleSheet.create({
   },
   homeSloganWare: {
     padding: 40,
+    textAlign: 'center',
+    flex: 1,
   },
   homeSlogan: {
     color: '#012B6D',
@@ -81,6 +72,19 @@ const styles = StyleSheet.create({
   homeAddUp: {
     color: '#012B6D',
     fontSize: 14,
+  },
+  writeBtnWare: {
+    flex: 1,
+    // justifyContent: 'center', // 垂直居中
+    alignItems: 'center', // 水平居中
+    // backgroundColor: 'blue',
+  },
+  writeBtn: {
+    // position: 'absolute',
+    bottom: px2dp(80),
+    // left: '50%',
+    // justifyContent: 'center',
+    // transform: 'translateX(-50%)',
   },
 });
 
